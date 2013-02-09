f384c713de19ff2b750550ea9c48df9dbf67dcf4
sniciq <sniciq@gmail.com>
2013-02-10 01:14:03 +0800

    短信群发

--- gen/com/eddy/firstapp/R.java
+++ gen/com/eddy/firstapp/R.java
@@ -11,24 +11,46 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int checked=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int icon=0x7f020002;
     }
     public static final class id {
-        public static final int edit_message=0x7f070000;
-        public static final int menu_settings=0x7f070001;
+        public static final int checked=0x7f070009;
+        public static final int contract=0x7f070002;
+        public static final int contractList=0x7f070000;
+        public static final int img=0x7f070006;
+        public static final int menu_settings=0x7f07000a;
+        public static final int message=0x7f070004;
+        public static final int mobile=0x7f070003;
+        public static final int phone=0x7f070008;
+        public static final int selectContractBtn=0x7f070001;
+        public static final int send=0x7f070005;
+        public static final int username=0x7f070007;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_contract=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int contract_item=0x7f030002;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_contract=0x7f060000;
+        public static final int activity_display_message=0x7f060001;
+        public static final int activity_main=0x7f060002;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int button_send=0x7f040004;
+        public static final int content=0x7f040007;
+        public static final int contract=0x7f040005;
         public static final int edit_message=0x7f040003;
+        public static final int hello=0x7f040006;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int ok=0x7f04000a;
+        public static final int send=0x7f040008;
+        public static final int title_activity_contract=0x7f04000b;
+        public static final int title_activity_display_message=0x7f040009;
     }
     public static final class style {
         /** 
